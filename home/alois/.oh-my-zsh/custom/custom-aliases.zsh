7c3659bd7baadff21d6a44ac9878858d94da174e
Alois Carrera <aloiscarrera@gmail.com>
2021-01-29 18:43:57 -0500

    feat: :sparkles: Add aliases for lsd and bat to replace ls and cat

---
+++ home/alois/.oh-my-zsh/custom/custom-aliases.zsh
@@ -0,0 +1,8 @@
+# Custom aliases overriding created ones by default
+
+alias ll='lsd -lh --group-dirs=first'
+alias la='lsd -a --group-dirs=first'
+alias l='lsd --group-dirs=first'
+alias lla='lsd -lha --group-dirs=first'
+alias ls='lsd --group-dirs=first'
+alias cat='bat'
