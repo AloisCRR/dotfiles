5592dc0414d08e58daadb1059e8621025ccd7c8c
Alois Carrera <aloiscarrera@gmail.com>
2021-01-31 19:21:11 -0500

    feat: :sparkles: Add custom functions to list package files and give colors to man pages

---
+++ home/alois/.oh-my-zsh/custom/custom-functions.zsh
@@ -0,0 +1,26 @@
+#!/usr/bin/zsh
+
+# Show all files associated with a package
+function package-files() {
+
+    if [ $# -eq 0 ]
+        then
+            >&2 printf "\nNo arguments provided\n\n"
+            return 1
+    fi
+
+    paru -Qlq "$@" | grep -v '/$' | xargs -r du -h | sort -h
+}
+
+# Custom colored man pages
+function man() {
+    env \
+    LESS_TERMCAP_mb=$'\e[01;31m' \
+    LESS_TERMCAP_md=$'\e[01;31m' \
+    LESS_TERMCAP_me=$'\e[0m' \
+    LESS_TERMCAP_se=$'\e[0m' \
+    LESS_TERMCAP_so=$'\e[01;44;33m' \
+    LESS_TERMCAP_ue=$'\e[0m' \
+    LESS_TERMCAP_us=$'\e[01;32m' \
+    man "$@"
+}
