c116fc2ca47725ba15cb9d7a1bbefacb77d64336
Alois Carrera <aloiscarrera@gmail.com>
2021-01-31 21:43:55 -0500

    feat: :sparkles: Add real solution to make fzf-tab show dotfiles and hidden folders

--- home/alois/.oh-my-zsh/custom/custom-zsh-options.zsh
+++ home/alois/.oh-my-zsh/custom/custom-zsh-options.zsh
@@ -5,4 +5,10 @@ setopt		BRACE_CCL
 setopt		GLOB_SUBST
 setopt		HIST_NO_STORE
 
+# List all hidden files in fzf-tab zsh plugin
+setopt	    GLOB_DOTS
+
+# To list both . and ..
+# setopt	GLOB_DOTS && zstyle ':completion:*' special-dirs true
+
 # TODO Remove expansion and add word correction
