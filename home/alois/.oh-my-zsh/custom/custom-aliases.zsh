bcc6fb904183f294bb4f255b8c84db19828c5fdb
Alois Carrera <aloiscarrera@gmail.com>
2021-02-01 18:28:21 -0500

    feat: :sparkles: Add alias to make a tmp directory and cd into it

--- home/alois/.oh-my-zsh/custom/custom-aliases.zsh
+++ home/alois/.oh-my-zsh/custom/custom-aliases.zsh
@@ -8,6 +8,7 @@ alias ls='lsd --group-dirs=first'
 alias cat='bat'
 alias paru-clean-install='paru --removemake --cleanafter --confirm'
 alias paru-list-packages="paru -Qq | fzf --preview 'paru -Qil {}' --preview-window=right:50%:wrap --layout=reverse --bind 'enter:execute(paru -Qil {} | less)'"
-alias paru-remove="paru -Rnsu"
-alias paru-remove-all="paru -Rnssuc --confirm"
-alias paru-info="paru -Sii"
+alias paru-remove='paru -Rnsu'
+alias paru-remove-all='paru -Rnssuc --confirm'
+alias paru-info='paru -Sii'
+alias temp='cd "$(mktemp -d)"'
