44d5faa0110fe1336b400d221a276e44a0db7d60
Alois Carrera <aloiscarrera@gmail.com>
2021-02-03 16:26:36 -0500

    feat: :sparkles: Add the "command not found" handler function

--- home/alois/.oh-my-zsh/custom/custom-functions.zsh
+++ home/alois/.oh-my-zsh/custom/custom-functions.zsh
@@ -11,6 +11,28 @@ function man() {
     man "$@"
 }
 
+# The "command not found" handler
+command_not_found_handler() {
+	local pkgs cmd="$1" files=()
+	printf 'zsh: command not found: %s' "$cmd" # print command not found asap, then search for packages
+	files=(${(f)"$(paru -F --machinereadable -- "/usr/bin/${cmd}")"})
+	if (( ${#files[@]} )); then
+		printf '\r%s may be found in the following packages:\n' "$cmd"
+		local res=() repo package version file
+		for file in "$files[@]"; do
+			res=("${(0)file}")
+			repo="$res[1]"
+			package="$res[2]"
+			version="$res[3]"
+			file="$res[4]"
+			printf '  %s/%s %s: /%s\n' "$repo" "$package" "$version" "$file"
+		done
+	else
+		printf '\n'
+	fi
+	return 127
+}
+
 # Override the default fzf-tab completion functions/completer
 # _files() {
 #     local files=($(fd --type f --hidden --follow --exclude .git))
