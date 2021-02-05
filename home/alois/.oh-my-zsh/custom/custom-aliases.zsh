8dc17cd5039d5302418501fc0d153cfe5405277c
Alois Carrera <aloiscarrera@gmail.com>
2021-02-05 08:58:32 -0500

    refactor: :sparkles: Add remove unnecessary dependencies to paru remove alias

--- home/alois/.oh-my-zsh/custom/custom-aliases.zsh
+++ home/alois/.oh-my-zsh/custom/custom-aliases.zsh
@@ -9,7 +9,7 @@ alias cat='bat'
 alias paru-clean-install='paru --removemake --cleanafter --confirm --bottomup'
 alias paru-list-packages="paru -Qq | fzf --preview 'paru -Qil {}' --preview-window=right:50%:wrap --layout=reverse --bind 'enter:execute(paru -Qil {} | less)'"
 alias paru-remove='paru -Rnsu'
-alias paru-remove-all='paru -Rnssuc --confirm'
+alias paru-remove-all='paru -Rnsssuc --confirm'
 alias paru-info='paru -Sii'
 alias temp='cd "$(mktemp -d)"'
 alias npm='pnpm'
